a767ae10e4c9857893b6bceaf7897a28cbeeb38d
Yves Yuen <yves.j.yuen@gmail.com>
2015-06-05 16:02:22 -0400

    Upload completed 'Build a Simple Dynamic Site with Node.js' workshop

---
+++ app.js
@@ -0,0 +1,11 @@
+var router = require("./router.js");
+
+//1. Create WebServer
+var http = require('http');
+http.createServer(function (request, response) {
+	router.home(request, response);
+	router.user(request, response);
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+
